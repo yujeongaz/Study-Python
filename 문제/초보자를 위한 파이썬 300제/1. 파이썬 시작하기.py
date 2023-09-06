b4cb8afbb09d8ec1dc2ca148ba0a1a6054c293a0
서유정 <azureapple1018@gmail.com>
2023-09-06 20:00:34 +0900

    Create : 1. 파이썬 시작하기

---
+++ 문제/초보자를 위한 파이썬 300제/1. 파이썬 시작하기.py
@@ -0,0 +1,30 @@
+# 1 ✅
+print("Hello world")
+
+# 2 ✅
+print("Mary's cosmetics")
+
+# 3 ✅
+print('신씨가 소리질렀다. "도둑이야".')
+
+# 4 ✅
+print("C:\Windows")
+
+# 5 ✅
+# \t: Tap, \n: Enter
+
+# 6 ✅
+# "오늘은 일요일"
+
+# 7 ✅
+# print("naver;kakao;sk;samsung")
+print("naver", "kakao", "sk", "samsung", sep=";")
+
+# 8 ✅
+print("naver", "kakao", "sk", "samsung", sep="/")
+
+# 9 ✅
+print("first", end=""); print("second")
+
+# 10
+print(5/3)
