9e84f019a035acfab3f13503b50dcaf8ec7a72d0
서유정 <azureapple1018@gmail.com>
2023-09-10 22:28:57 +0900

    Update : 3. 파이썬 문자열

--- 문제/초보자를 위한 파이썬 300제/3. 파이썬 문자열.py
+++ 문제/초보자를 위한 파이썬 300제/3. 파이썬 문자열.py
@@ -36,3 +36,46 @@ print(string.replace("a", "A"))
 # 30 ✅
 # "aBcd" ❌
 # "abcd"
+
+# 31 ✅
+# "34"
+
+# 32 ✅
+# "HiHiHi"
+
+# 33 ✅
+print("-" * 80)
+
+# 34 ✅
+t1 = "python"
+t2 = "java"
+print((t1 + " " + t2 + " ") * 4)
+
+# 35 ✅
+name1 = "김민수"
+age1 = 10
+name2 = "이철희"
+age2 = 13
+print("이름: %s 나이: %d" % (name1, age1))
+print("이름: %s 나이: %d" % (name2, age2))
+
+# 36 ✅
+print("이름: {0} 나이: {1}".format(name1, age1))
+print("이름: {0} 나이: {1}".format(name2, age2))
+
+# 37 ✅
+print(f"이름: {name1} 나이: {age1}")
+print(f"이름: {name2} 나이: {age2}")
+
+# 38 ✅
+상장주식수 = "5,969,782,550"
+상장주식수 = int(상장주식수.replace(",", ""))
+print(상장주식수, type(상장주식수))
+
+# 39 ✅
+분기 = "2020/03(E) (IFRS연결)"
+print(분기[:7])
+
+# 40 ✅
+data = "   삼성전자    "
+print(data.strip())
