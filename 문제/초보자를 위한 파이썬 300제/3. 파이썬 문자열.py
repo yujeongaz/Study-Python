17a35a7a93724b64fd63773f37bbd81e4b811b4e
서유정 <azureapple1018@gmail.com>
2023-09-11 00:49:22 +0900

    Update : 3. 파이썬 문자열

--- 문제/초보자를 위한 파이썬 300제/3. 파이썬 문자열.py
+++ 문제/초보자를 위한 파이썬 300제/3. 파이썬 문자열.py
@@ -79,3 +79,42 @@ print(분기[:7])
 # 40 ✅
 data = "   삼성전자    "
 print(data.strip())
+
+# 41 ✅
+ticker = "btc_krw"
+print(ticker.upper())
+
+# 42 ✅
+ticker = "BTC_KRW"
+print(ticker.lower())
+
+# 43 ✅
+str = "hello"
+print(str.capitalize())
+
+# 44 ✅
+file_name = "보고서.xlsx"
+print(file_name.endswith("xlsx"))
+
+# 45 ✅
+print(file_name.endswith(("xlsx", "xls")))
+
+# 46 ✅
+file_name = "2020_보고서.xlsx"
+print(file_name.startswith("2020"))
+
+# 47 ✅
+a = "hello world"
+print(a.split(" "))
+
+# 48 ✅
+ticker = "btc_krw"
+print(ticker.split("_"))
+
+# 49 ✅
+date = "2020-05-01"
+print(date.split("-"))
+
+# 50
+data = "039490     "
+print(data.rstrip())
