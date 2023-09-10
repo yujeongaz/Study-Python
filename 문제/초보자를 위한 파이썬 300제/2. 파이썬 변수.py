f87ce0d1b410512ed265c4e4251576485bfdf4ad
서유정 <azureapple1018@gmail.com>
2023-09-10 21:36:02 +0900

    Create : 2. 파이썬 변수

---
+++ 문제/초보자를 위한 파이썬 300제/2. 파이썬 변수.py
@@ -0,0 +1,52 @@
+# 11 ✅
+삼성전자 = 50000
+총평가금액 = 삼성전자 * 10
+print(총평가금액)
+
+# 12 ✅
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+
+# 13 ✅
+s = "hello"
+t = "python"
+print(f"{s}! {t}")
+
+# 14 ✅
+# 8
+
+# 15 ✅
+a = "132"
+print(type(a))
+
+# 16 ✅
+num_str = "720"
+num_int = int(num_str)
+print(num_int, type(num_int))
+
+# 17 ✅
+num = 100
+str = str(num)
+print(str, type(str))
+
+# 18 ✅
+str = "15.79"
+float = float(str)
+print(float, type(float))
+
+# 19 ✅
+year = "2020"
+year = int(year)
+print(year - 1) # 2019
+print(year - 2) # 2018
+print(year - 3) # 2017
+
+# 20 ✅
+월 = 48584
+총금액 = 월 * 36
+print(총금액)
