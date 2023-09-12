53dc783df69ea830e90e9fe4e155013e5ba6b3d0
서유정 <azureapple1018@gmail.com>
2023-09-12 19:42:03 +0900

    Create : 5. 파이썬 튜플

---
+++ 문제/초보자를 위한 파이썬 300제/5. 파이썬 튜플.py
@@ -0,0 +1,37 @@
+# 71 ✅
+my_variable = ()
+print(type(my_variable))
+
+# 72 ✅
+movie_rank = ("닥터 스트레인지", "스플릿", "럭키")
+print(movie_rank)
+
+# 73 ✅
+number1 = (1, )
+print(type(number1))
+
+# 74 ✅
+# 튜플은 리스트가 아니기 때문에 이미 할당된 원소는 수정할 수 없다.
+
+# 75 ✅
+# 자료형: 튜플
+
+# 76 ✅
+t = ("a", "b", "c")
+t = ("A", "b", "c")
+print(t)
+
+# 77 ✅
+interest = ("삼성전자", "LG전자", "SK Hynix")
+print(list(interest))
+
+# 78 ✅
+interest = ["삼성전자", "LG전자", "SK Hynix"]
+print(tuple(interest))
+
+# 79 ✅
+# "apple" "banana" "cake"
+
+# 80 ✅
+data = tuple(range(2, 99, 2))
+print(data)
