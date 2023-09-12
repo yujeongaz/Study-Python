27dcb095967682827d7e2cd5ec052485ba35adf9
서유정 <azureapple1018@gmail.com>
2023-09-12 22:03:38 +0900

    Update : 6. 파이썬 딕셔너리

--- 문제/초보자를 위한 파이썬 300제/6. 파이썬 딕셔너리.py
+++ 문제/초보자를 위한 파이썬 300제/6. 파이썬 딕셔너리.py
@@ -41,3 +41,49 @@ print(product)
 
 # 90 ✅
 # icecream의 없는 키를 사용해서 인덱싱을 했기 때문이다.
+
+# 91 ✅
+inventory = {
+    "메로나": [300, 20],
+    "비비빅": [400, 3],
+    "죠스바": [250, 100],
+}
+
+# 92 ✅
+print(inventory["메로나"][0])
+
+# 93 ✅
+print(inventory["메로나"][1])
+
+# 94 ✅
+inventory["월드콘"] = [500, 7]
+
+# 95 ✅
+icecream = {'탱크보이': 1200, '폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+arr = list(icecream.keys())
+print(arr)
+
+# 96 ✅
+arr = list(icecream.values())
+print(arr)
+
+# 97 ✅
+print(sum(arr))
+
+# 98 ✅
+new_product = {
+    "팥빙수": 2700,
+    "아맛나": 1000,
+}
+icecream.update(new_product)
+print(icecream)
+
+# 99 ✅
+keys = ("apple", "pear", "peach")
+vals = (300, 250, 400)
+print(dict(zip(keys, vals)))
+
+# 100 ✅
+date = ['09/05', '09/06', '09/07', '09/08', '09/09']
+close_price = [10500, 10300, 10100, 10800, 11000]
+print(dict(zip(date, close_price)))
